8bf80463197e8db6621591be58fd556dfeab5ab5
LizaZayats <liza2007zayats@gmail.com>
2025-10-08 19:38:34 +0300

    Remove 4Bii.js from repository (keep local)

--- L4/4Bii.js
+++
@@ -1,9 +0,0 @@
-const todos = pm.response.json();
-pm.response.json = () => incompleteTodos;
-
-pm.test(Status 200, () => pm.expect(pm.response.code).to.eql(200));
-pm.test(All todos are incomplete, () => {
-    pm.response.json().forEach(todo => pm.expect(todo.completed).to.be.false);
-});
-
-console.log();
