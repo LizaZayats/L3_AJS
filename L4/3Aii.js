c54a5a05cd3a6d54876873c4616bc8d14dc300c1
LizaZayats <liza2007zayats@gmail.com>
2025-10-08 19:24:21 +0300

    Remove 3Aii.js file

--- L4/3Aii.js
+++
@@ -1,8 +0,0 @@
-
-pm.test(Status 200, () => pm.expect(pm.response.code).to.eql(200));
-pm.test(Sorted by name, () => {
-    const comments = pm.response.json();
-    for (let i = 0; i < comments.length-1; i++) {
-        pm.expect(comments[i].name <= comments[i+1].name).to.be.true;
-    }
-});
